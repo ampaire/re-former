d041313660f98d8710b5216d659c2eb115c41bc1
ampaire <femayapr@gmail.com>
2019-12-20 15:20:09 -0600

    Created the user resource in the routes so that the requests have somewhere to go. I added :only option to specify the :new and :create actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get  '/signup',  to: 'users#create'
-  get  '/signup',  to: 'users#new'
-  resources :users
+  resources :users, :only => [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
