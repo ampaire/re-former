aef238fe6410256cc4cc96278b8e326b0643f236
rOluochKe <rasholuoch@gmail.com>
2020-01-06 20:56:49 +0300

    working with form submission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,18 @@
 class UsersController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    # @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name,:email,:password)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_20_210123) do
+ActiveRecord::Schema.define(version: 2020_01_06_164634) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_12_20_210123) do
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
