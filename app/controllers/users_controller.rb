c719b2ba13c4eb648be768e545d1e08c7c89babc
ampaire <femayapr@gmail.com>
2020-01-06 15:49:29 -0600

    Added the edit and update actions to the form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,26 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to user_path(@user) 
     else
       render :new
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
   def user_params
     params.require(:user).permit(:name, :email, :password)
   end
