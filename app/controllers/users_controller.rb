9ee46ef4bd43be4555d6a84e9c2db57dc959b412
ampaire <femayapr@gmail.com>
2020-01-07 13:12:26 -0600

    Implemented the feedback from the TSE,

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to user_path(@user)
+      redirect_to new_user_path(@user)
     else
       render :new
     end
